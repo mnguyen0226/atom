5dcb83b91e3c78b7084c0299d6f5d1ea8d08f3d3
mnguyen0226 <mnguyen0226@vt.edu>
2024-06-13 22:53:01 -0400

    Added another role

--- src/app.py
+++ src/app.py
@@ -7,7 +7,10 @@ from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-with open("roles/orderbot.txt", "r") as file:
+role = "harvard_advisor"
+# role = "orderbot"
+
+with open(f"roles/{role}.txt", "r") as file:
     role_content = file.read()
 
 context_dict = {
@@ -15,6 +18,7 @@ context_dict = {
     "content": role_content,
 }
 
+# Initialize session state if not already initialized
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
 
@@ -25,11 +29,24 @@ if "messages" not in st.session_state:
 st.sidebar.title("A.T.O.M")
 st.sidebar.markdown("International student assistant in the U.S.")
 
+
+# Function to reset the conversation
+def reset_conversation():
+    st.session_state.messages = [context_dict]
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+
+# Display reset button in the sidebar
+if st.sidebar.button("Reset Conversation"):
+    reset_conversation()
+
+# Display messages
 for message in st.session_state.messages:
     if message["role"] != "system":
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
+# User input and assistant response logic
 if prompt := st.chat_input("Enter your message here..."):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
